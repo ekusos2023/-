ebe4ba0aa96a530a2dd10271ce45ded3d58c2f44
sosukekai <123486289+ekusos2023@users.noreply.github.com>
2024-10-30 11:52:59 +0900

    Add files via upload

---
+++ import pyautogui.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+time.sleep(10)
+x, y =pyautogui.position()
+print(f"マウスの位置: X{x}, Y{y}")
+
+import pyautogui
+import time
+
+def auto_click(x, y, interval=6):
+    while True:
+        pyautogui.rightClick(x, y)
+        time.sleep(interval)
+          
+auto_click(477,176) 
